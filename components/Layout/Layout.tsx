446ab2ce131ca49ea5dc247a6abf901a2b32e2c9
Jason <jsnswny@gmail.com>
2023-06-12 19:33:20 +0100

    Added favicon

--- components/Layout/Layout.tsx
+++ components/Layout/Layout.tsx
@@ -33,8 +33,12 @@ const Layout = ({ children }: LayoutProps) => {
             href="https://fonts.googleapis.com/css2?family=Newsreader:wght@300;400;500;600;700;800&family=Raleway:ital,wght@0,300;0,400;0,500;0,700;1,400;1,700&display=swap"
             rel="stylesheet"
           />
+          <link
+            rel="shortcut icon"
+            href="https://coreer-static.s3.eu-west-2.amazonaws.com/media/favicon/favicon.png"
+          />
           <script
-          async
+            async
             src={`https://maps.googleapis.com/maps/api/js?key=${apiKey}&libraries=places`}
           ></script>
         </Head>
