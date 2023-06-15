b8756e889eb1ed2ef20cf058388ffe3a0abf0b12
Sean Paterson <seanpaterson_25@msn.com>
2023-06-15 11:29:19 +0100

    fix: update crossorigin to allow anonymous

--- components/Layout/Layout.tsx
+++ components/Layout/Layout.tsx
@@ -28,7 +28,7 @@ const Layout = ({ children }: LayoutProps) => {
       <div className={styles.container}>
         <Head>
           <link rel="preconnect" href="https://fonts.googleapis.com" />
-          <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+          <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
           <link
             href="https://fonts.googleapis.com/css2?family=Newsreader:wght@300;400;500;600;700;800&family=Raleway:ital,wght@0,300;0,400;0,500;0,700;1,400;1,700&display=swap"
             rel="stylesheet"
