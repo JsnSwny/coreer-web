0b3e49069b1fb35a4f3be7ec4d6108edefb0caa5
Jason <jsnswny@gmail.com>
2023-06-26 19:20:58 +0100

    feat: updated favicon; added logo to header;

--- components/Header/Header/Header.tsx
+++ components/Header/Header/Header.tsx
@@ -1,8 +1,8 @@
 import { useAuth } from "@/contexts/AuthContext";
 import {
-	faEnvelope,
-	faFileCirclePlus,
-	faStar,
+  faEnvelope,
+  faFileCirclePlus,
+  faStar,
 } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import Link from "next/link";
@@ -14,46 +14,47 @@ import Search from "../Search/Search";
 import styles from "./Header.module.scss";
 
 const Header = () => {
-	const router = useRouter();
-	const largeContainer = router.pathname == "/";
+  const router = useRouter();
+  const largeContainer = router.pathname == "/";
 
-	const { user } = useAuth();
+  const { user } = useAuth();
 
-	return (
-		<header className={styles.header}>
-			<Container size={largeContainer ? "large" : "medium"}>
-				<div className={styles.wrapper}>
-					<Nav>
-						<Link href="/">
-							<p className={styles.title}>
-								<span>co</span>reer
-							</p>
-						</Link>
-						<Search />
-					</Nav>
+  return (
+    <header className={styles.header}>
+      <Container size={largeContainer ? "large" : "medium"}>
+        <div className={styles.wrapper}>
+          <Nav>
+            <Link href="/" className={styles.logo}>
+              <img src="https://coreer-static.s3.eu-west-2.amazonaws.com/media/favicon/favicon.svg" />
+              <p className={styles.title}>
+                <span>co</span>reer
+              </p>
+            </Link>
+            <Search />
+          </Nav>
 
-					<div className={styles.right}>
-						{user ? (
-							<>
-								{/* <Link href="/likes" className={styles.icon}>
+          <div className={styles.right}>
+            {user ? (
+              <>
+                {/* <Link href="/likes" className={styles.icon}>
 									<FontAwesomeIcon icon={faFileCirclePlus} />
 								</Link>
 
 								<Link href="/messages" className={styles.icon}>
 									<FontAwesomeIcon icon={faEnvelope} />
 								</Link> */}
-								<NavProfile />
-							</>
-						) : (
-							<Link href="/login" className={styles.icon}>
-								Login
-							</Link>
-						)}
-					</div>
-				</div>
-			</Container>
-		</header>
-	);
+                <NavProfile />
+              </>
+            ) : (
+              <Link href="/login" className={styles.icon}>
+                Login
+              </Link>
+            )}
+          </div>
+        </div>
+      </Container>
+    </header>
+  );
 };
 
 export default Header;
