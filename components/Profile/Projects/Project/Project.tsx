214f42472f70ed0a2da3468a3af234646e307828
JsnSwny <jsnswny@gmail.com>
2023-06-26 13:18:48 +0100

    fix: tweaks to project styling

--- components/Profile/Projects/Project/Project.tsx
+++ components/Profile/Projects/Project/Project.tsx
@@ -85,52 +85,54 @@ const Project = ({
 					</div>
 				)}
 			</div>
-			{!isProfile && (
-				<Link
-					href={`/${project.user.username}`}
-					className={styles.profile}
-					onClick={(e) => e.stopPropagation()}
-				>
-					<img
-						alt="Project creator profile picture"
-						src={project.user.image}
-						className={styles.profileImage}
-					/>
-					<p>
-						{project.user.first_name} {project.user.last_name}
+			<div className={styles.contentContainer}>
+				{!isProfile && (
+					<Link
+						href={`/${project.user.username}`}
+						className={styles.profile}
+						onClick={(e) => e.stopPropagation()}
+					>
+						<img
+							alt="Project creator profile picture"
+							src={project.user.image}
+							className={styles.profileImage}
+						/>
+						<p>
+							{project.user.first_name} {project.user.last_name}
+						</p>
+					</Link>
+				)}
+				{isProfile && project.start_date && (
+					<p className={styles.date}>
+						{`${format(parseISO(project.start_date), "MMMM yyyy")} - ${
+							project.end_date
+								? format(parseISO(project.end_date), "MMMM yyyy")
+								: "Present"
+						}`}{" "}
+						({calculateTimeDifference(project.start_date, project.end_date)})
 					</p>
-				</Link>
-			)}
-			{isProfile && project.start_date && (
-				<p className={styles.date}>
-					{`${format(parseISO(project.start_date), "MMMM yyyy")} - ${
-						project.end_date
-							? format(parseISO(project.end_date), "MMMM yyyy")
-							: "Present"
-					}`}{" "}
-					({calculateTimeDifference(project.start_date, project.end_date)})
-				</p>
-			)}
-			<div className={styles.header}>
-				<h3 className={styles.title}>{project.title}</h3>
-				{/* {project.description && (
+				)}
+				<div className={styles.header}>
+					<h3 className={styles.title}>{project.title}</h3>
+					{/* {project.description && (
 					<p className={styles.description}>
 						{project.description.replace(/<[^>]+>/g, "")}
 					</p>
 				)} */}
-			</div>
+				</div>
 
-			{project.languages && project.languages.length > 0 && (
-				<TagsList
-					tags={project.languages.map((item) => ({
-						text: item.name,
-						highlight: user
-							? user?.languages.some((lang) => item.id == lang.id)
-							: true,
-					}))}
-					fade
-				/>
-			)}
+				{project.languages && project.languages.length > 0 && (
+					<TagsList
+						tags={project.languages.map((item) => ({
+							text: item.name,
+							highlight: user
+								? user?.languages.some((lang) => item.id == lang.id)
+								: true,
+						}))}
+						fade
+					/>
+				)}
+			</div>
 		</div>
 	);
 };
