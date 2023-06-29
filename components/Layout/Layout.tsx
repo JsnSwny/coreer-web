e4afd5cd322ee674f11ab85ee12630ec8115acd9
JsnSwny <jsnswny@gmail.com>
2023-06-29 09:49:40 +0100

    feat: added google tag manager script

--- components/Layout/Layout.tsx
+++ components/Layout/Layout.tsx
@@ -7,56 +7,57 @@ import Header from "../Header/Header/Header";
 import styles from "./Layout.module.scss";
 
 import "react-toastify/dist/ReactToastify.css";
+import Script from "next/script";
 
 interface LayoutProps {
-  children: React.ReactNode;
+	children: React.ReactNode;
 }
 
 const Layout = ({ children }: LayoutProps) => {
-  const apiKey = process.env.NEXT_PUBLIC_GOOGLE_PLACES_API_KEY;
-  const { user } = useAuth();
-  const router = useRouter();
-  const [showHeader, setShowHeader] = useState(user && user.onboarded);
+	const apiKey = process.env.NEXT_PUBLIC_GOOGLE_PLACES_API_KEY;
+	const { user } = useAuth();
+	const router = useRouter();
+	const [showHeader, setShowHeader] = useState(user && user.onboarded);
 
-  useEffect(() => {
-    setShowHeader(user && user.onboarded);
-  }, [user, router]);
+	useEffect(() => {
+		setShowHeader(user && user.onboarded);
+	}, [user, router]);
 
-  return (
-    <>
-      <div className={styles.container}>
-        <Head>
-          <link rel="preconnect" href="https://fonts.googleapis.com" />
-          <link
-            rel="preconnect"
-            href="https://fonts.gstatic.com"
-            crossOrigin="anonymous"
-          />
-          <link
-            href="https://fonts.googleapis.com/css2?family=Arimo:wght@400;700&family=Raleway:wght@300;400;700;900&display=swap"
-            rel="stylesheet"
-          />
+	return (
+		<>
+			<div className={styles.container}>
+				<Head>
+					<link rel="preconnect" href="https://fonts.googleapis.com" />
+					<link
+						rel="preconnect"
+						href="https://fonts.gstatic.com"
+						crossOrigin="anonymous"
+					/>
+					<link
+						href="https://fonts.googleapis.com/css2?family=Arimo:wght@400;700&family=Raleway:wght@300;400;700;900&display=swap"
+						rel="stylesheet"
+					/>
 
-          <link
-            rel="shortcut icon"
-            href="https://coreer-static.s3.eu-west-2.amazonaws.com/media/favicon/favicon.svg"
-          />
-          <script
-            async
-            src={`https://maps.googleapis.com/maps/api/js?key=${apiKey}&libraries=places`}
-          ></script>
+					<link
+						rel="shortcut icon"
+						href="https://coreer-static.s3.eu-west-2.amazonaws.com/media/favicon/favicon.svg"
+					/>
+					<script
+						async
+						src={`https://maps.googleapis.com/maps/api/js?key=${apiKey}&libraries=places`}
+					></script>
 
-          <meta name="viewport" content="width=device-width, initial-scale=1" />
-        </Head>
-        {/* <Sidebar /> */}
-        <ToastContainer />
-        <div className={styles.content}>
-          {!router.pathname.includes("/onboarding") && <Header />}
-          <main>{children}</main>
-        </div>
-      </div>
-    </>
-  );
+					<meta name="viewport" content="width=device-width, initial-scale=1" />
+				</Head>
+				{/* <Sidebar /> */}
+				<ToastContainer />
+				<div className={styles.content}>
+					{!router.pathname.includes("/onboarding") && <Header />}
+					<main>{children}</main>
+				</div>
+			</div>
+		</>
+	);
 };
 
 export default Layout;
--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,13 +1,25 @@
-import { Html, Head, Main, NextScript } from 'next/document'
+import { Html, Head, Main, NextScript } from "next/document";
+import Script from "next/script";
 
 export default function Document() {
-  return (
-    <Html lang="en">
-      <Head />
-      <body>
-        <Main />
-        <NextScript />
-      </body>
-    </Html>
-  )
+	return (
+		<Html lang="en">
+			<Head />
+			<body>
+				<Script
+					id="tag-manager"
+					strategy="afterInteractive"
+					dangerouslySetInnerHTML={{
+						__html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id=G-WR67K5QVL0'+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GT-5M8N3BN');`,
+					}}
+				></Script>
+				<Main />
+				<NextScript />
+			</body>
+		</Html>
+	);
 }
