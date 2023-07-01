1a40ebf65d6a80cbfe99b57cf21d9bf1400dd8cb
Jason <jsnswny@gmail.com>
2023-07-01 17:20:43 +0100

    fix: adjusted google analytics tag

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -2,24 +2,23 @@ import { Html, Head, Main, NextScript } from "next/document";
 import Script from "next/script";
 
 export default function Document() {
-	return (
-		<Html lang="en">
-			<Head />
-			<body>
-				<Script
-					id="tag-manager"
-					strategy="afterInteractive"
-					dangerouslySetInnerHTML={{
-						__html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-'https://www.googletagmanager.com/gtm.js?id=G-WR67K5QVL0'+i+dl;f.parentNode.insertBefore(j,f);
-})(window,document,'script','dataLayer','GT-5M8N3BN');`,
-					}}
-				></Script>
-				<Main />
-				<NextScript />
-			</body>
-		</Html>
-	);
+  return (
+    <Html lang="en">
+      <Head />
+      <body>
+        <Script src="https://www.googletagmanager.com/gtag/js?id=G-WR67K5QVL0" />
+        <Script id="google-analytics">
+          {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+
+          gtag('config', 'G-WR67K5QVL0');
+        `}
+        </Script>
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  );
 }
